6bc6fc0b72781c5d76208ac2fa1110779c79055f
Boranna, Chethan | ECMPD <ts-chethan.a.boranna@rakuten.com>
2021-04-12 10:00:08 +0900

    Changes : Fixed Testcases

--- app/src/test/java/com/example/tipjar/viewmodel/HistoryFragmentViewModelTest.kt
+++ app/src/test/java/com/example/tipjar/viewmodel/HistoryFragmentViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.example.tipjar.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import com.example.tipjar.database.dao.TipJarDao
 import com.example.tipjar.database.entity.TipHistory
 import com.example.tipjar.model.Calculator
 import com.example.tipjar.repository.TipJarRepository
@@ -24,6 +26,9 @@ class HistoryFragmentViewModelTest {
     @Mock
     private lateinit var mockCalculator: Calculator
 
+    @Mock
+    private lateinit var tipJarDao: TipJarDao
+
     @Rule
     @JvmField
     val instantExecutorRule = InstantTaskExecutorRule()
@@ -39,10 +44,38 @@ class HistoryFragmentViewModelTest {
 
     @Test
     fun basic() {
-        val result = mock<Observer<List<TipHistory>>>()
-        historyFragmentViewModel.loadTipHistory().observeForever(result)
+        val observer = mock<Observer<List<TipHistory>>>()
+        val dbResult = MutableLiveData<List<TipHistory>>()
 
-        // check it call - repository.loadAllTheSavedTips()
-        Mockito.verify(repository).loadAllTheSavedTips()
+        val baseTipCalculation = TipHistory(inputtedAmount = 10.0)
+
+        val testValues = listOf(
+            baseTipCalculation.copy(
+                inputtedTipInPercent = 25.0,
+                totalTipAmount = 2.5,
+                grandTotal = 12.50, tipPerPerson = 2.5, timestamp = System.currentTimeMillis()
+            ),
+            baseTipCalculation.copy(
+                inputtedTipInPercent = 15.0,
+                totalTipAmount = 1.5,
+                grandTotal = 11.50, tipPerPerson = 1.5, timestamp = System.currentTimeMillis()
+            ),
+            baseTipCalculation.copy(
+                inputtedTipInPercent = 18.0,
+                totalTipAmount = 1.8,
+                grandTotal = 11.80, tipPerPerson = 1.8, timestamp = System.currentTimeMillis()
+            )
+        )
+
+        // mocking method to return table result
+        Mockito.`when`(historyFragmentViewModel.loadTipHistory()).thenReturn(dbResult)
+
+        historyFragmentViewModel.loadTipHistory().observeForever(observer)
+
+        // posting tip data
+        dbResult.postValue(testValues)
+
+        // Finally , Check received object is same posted value
+        Mockito.verify(observer).onChanged(testValues)
     }
 }
